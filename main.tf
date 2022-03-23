0639d1f47b47f0969e581d5a78b53a743f8ae10c
bayupw <bayupw@gmail.com>
2022-03-23 17:37:27 +1300

    update vars

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 # Create random string
 resource "random_string" "this" {
-  length  = 3
+  length  = var.random_string_length
   special = false
   upper   = false
 }
 
+# Create IAM Role
 resource "aws_iam_role" "this" {
   name               = var.random_suffix == true ? "${var.ssm_instance_role_name}-${random_string.this.id}" : var.ssm_instance_role_name
   assume_role_policy = <<EOF
@@ -19,11 +20,13 @@ resource "aws_iam_role" "this" {
 EOF
 }
 
+# Create IAM Instance Profile
 resource "aws_iam_instance_profile" "this" {
   name = var.random_suffix == true ? "${var.ssm_instance_profile_name}-${random_string.this.id}" : var.ssm_instance_profile_name
   role = aws_iam_role.this.name
 }
 
+# Create IAM Role Policy Attachment
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
+# Boolean to determine if name will be appended with random strings
 variable "random_suffix" {
   type        = bool
   default     = true
   description = "Add random suffix"
 }
 
+# Length of random strings to be appended to the name
 variable "random_string_length" {
   type        = number
   default     = 3
   description = "Random string length"
 }
 
+# SSM IAM Role name
 variable "ssm_instance_role_name" {
   type        = string
-  default     = "ssm-instance-role"
-  description = "SSM instance role name"
+  default     = "ssm-role"
+  description = "SSM role name"
 }
 
+# SSM IAM Instance Profile name
 variable "ssm_instance_profile_name" {
   type        = string
   default     = "ssm-instance-profile"
